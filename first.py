cd56ba5c178a0fe640935322c9feaf6d7420cb88
Abhinav Singh Bhardwaj <95396382+ab22032002@users.noreply.github.com>
2022-03-28 20:03:50 +0530

    f

--- first.py
+++ first.py
@@ -1,4 +1,17 @@
+# modules are already existing external codes we can jsut import it
+# pip is package manager of pyhton
+# build in modules and imported modules
+# random is built in
+# pandas and flask we have to add it through the
+# pip install flask
+# no_filename match the module_name
+# import flask
+# even don't put a space between what you want , not even in the new line writting
+ # import pandas  // generating errors as its not installed
+print("hello_world_python ")
 a = 500
 print(a)
 print("this is the first python file ")
 print("this is the jeet verma show ")
+
+
